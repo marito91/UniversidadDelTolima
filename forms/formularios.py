376f229b00b40f176d022cf056620bd63230c889
ajosearroyo <91162987+ajosearroyo@users.noreply.github.com>
2021-10-27 13:23:29 -0500

    2 mejoroas de inicio de sesion

--- forms/formularios.py
+++ forms/formularios.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 class Login(FlaskForm):
     username = IntegerField("Identificación 👥", validators = [DataRequired("Por favor llene este campo con numeros")])
     password = PasswordField("  Password  🔑", validators = [DataRequired("Por favor llene este campo")])
-    entrar = SubmitField("Entrar")
+    entrar = SubmitField("Entrar 📡")
 
 
 class Registro(FlaskForm):
